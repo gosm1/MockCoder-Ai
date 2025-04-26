c37a2027a6e1072ce0d1b0364924361f0aca9ac4
elgoss <mohcineelgoss0@gmail.com>
2025-04-26 15:54:47 +0100

    fixing deployment errors v1

--- lib/vapi.sdk.ts
+++ lib/vapi.sdk.ts
@@ -1,4 +1,3 @@
-import Vapi from "@vapi-ai/web";
+// import Vapi from "@vapi-ai/web";
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const vapi = new Vapi(process.env.VAPI_API_KEY!);
+// const vapi = new Vapi(process.env.VAPI_API_KEY!);
